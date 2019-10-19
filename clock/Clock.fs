f9969778945e628c131e35b85050d7ab19974a2a
Anthony Guérot <anthony@digikare.com>
2019-10-19 21:43:06 +0200

    clock completed

--- clock/Clock.fs
+++ clock/Clock.fs
@@ -1,9 +1,20 @@
 module Clock
 
-let create hours minutes = failwith "You need to implement this function."
+let day = 24 * 60
 
-let add minutes clock = failwith "You need to implement this function."
+let private normalise minutes =
+    (minutes % day + day) % day // add + modulo day length to accomodate < 0h and > 24h overflow
 
-let subtract minutes clock = failwith "You need to implement this function."
+let create hours minutes =
+    hours * 60 + minutes |> normalise
 
-let display clock = failwith "You need to implement this function."
+let add minutes clock =
+    clock + minutes |> normalise
+
+let subtract minutes clock =
+    add (- minutes) clock
+
+let display clock =
+    let minutes = clock % 60
+    let hours = clock / 60
+    sprintf "%02d:%02d" hours minutes
