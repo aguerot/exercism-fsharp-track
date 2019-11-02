a9e17545eb6676d8eaeb8272ec699b49499a264f
Anthony Guérot <anthony@digikare.com>
2019-11-02 14:13:25 +0100

    refactoring leap

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -2,10 +2,6 @@
 
 
 let leapYear (year: int): bool =
-    let isDivisibleBy (divisor: int): bool = year % divisor = 0
+    let isDivisibleBy divisor = year % divisor = 0
 
-    match year with
-        | year when isDivisibleBy(400) -> true
-        | year when isDivisibleBy(100) -> false
-        | year when isDivisibleBy(4) -> true
-        | _ -> false 
+    isDivisibleBy(400) || not (isDivisibleBy(100)) && isDivisibleBy(4)
