fc931fd2550db135dee44f650ed48b6cd4f379d2
Anthony Guérot <anthony@digikare.com>
2019-11-02 13:58:04 +0100

    fix twofer w/ feedback

--- two-fer/TwoFer.fs
+++ two-fer/TwoFer.fs
@@ -1,9 +1,7 @@
 ﻿module TwoFer
 
-let composeOutput (input: string): string =
-    "One for " + input + ", one for me."
+let composeOutput input =
+    sprintf "One for %s, one for me." input
 
 let twoFer (input: string option): string = 
-    match input with
-        | Some name -> composeOutput(name)
-        | None -> composeOutput("you") 
+    input |> Option.defaultValue "you" |> composeOutput
